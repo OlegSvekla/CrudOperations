2ab1d6b96b7e25aa5b1da7e817a4ca5ef0e097b9
OlegSvekla <olegsvekla993@gmail.com>
2023-10-05 19:16:18 +0300

    add user and roles entities

---
+++ src/CrudOperations.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CrudOperations.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
